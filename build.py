f0a2fa8bda8ad007616c55a5be834812f29eee92
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2024-11-25 19:10:09 -0300

    Remove hardcoded year from title

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ TEMPLATE = """\
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Amigo Secreto 2023</title>
+    <title>Amigo Secreto</title>
     <style>
 body {{
 	display: flex;
